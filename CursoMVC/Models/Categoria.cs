02e2b9cd33713ab5c20316211912dd243942368c
vinibenfatti <vinibenfatti@gmail.com>
2021-02-02 11:34:42 -0300

    Adicionar arquivos de projeto.

---
+++ CursoMVC/Models/Categoria.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatorio")]
+        public string Descricao { get; set; }
+
+    }
+}
---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
+        }
+    }
+}
